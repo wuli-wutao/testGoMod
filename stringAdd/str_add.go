fb24dcdef87cece6a6a096b06a6d2aa72896e18c
伍涛 <wutao.buaa@bytedance.com>
2020-03-11 16:08:39 +0800

    add new

--- stringAdd/str_add.go
+++ stringAdd/str_add.go
@@ -2,6 +2,15 @@ package stringAdd
 import (
 	"fmt"
 )
-func Add(name string) string{
-	return fmt.Sprintf("Hello, %s !!!", name)
+func Add(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "zh":
+		return fmt.Sprintf("你好, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", fmt.Errorf("unknow language")
+	}
 }
